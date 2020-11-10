1d634b4be2084a54a82833eb8b8ea7831a804297
Ralf Sternberg <rsternberg@eclipsesource.com>
2020-11-10 22:46:21 +0100

    Support static files for frontend
    Add a handler that tries to resolve all unknown URL paths as static
    files in the `www` folder.
    A frontend is being developed in a separate project and copied into this
    folder.

--- mopidy_kitchen/web.py
+++ mopidy_kitchen/web.py
@@ -9,11 +9,11 @@ def webapp_factory(config, core):
     albums_dir = Extension.get_albums_dir(config)
     www_dir = Path(__file__).parent / "www"
     return [
-        (r"/(index.html)?", IndexHandler, {"path": www_dir}),
-        (r"/albums/(.+)", tornado.web.StaticFileHandler, {"path": albums_dir}),
+        (r"/albums/(.+)", FileHandler, {"path": albums_dir}),
+        (r"/(.*)", FileHandler, {"path": www_dir}),
     ]
 
 
-class IndexHandler(tornado.web.StaticFileHandler):
+class FileHandler(tornado.web.StaticFileHandler):
     def parse_url_path(self, url_path: str) -> str:
-        return super().parse_url_path("index.html")
+        return super().parse_url_path(url_path or "index.html")
